b19f5750bb3e3132b7a074f3d4795808709f080b
ItacosI <47180816+ItacosI@users.noreply.github.com>
2020-01-20 16:33:52 +0100

    Update grille.ml

--- All/grille.ml
+++ All/grille.ml
@@ -1,11 +1,20 @@
+(*Ouverture de fichier et lecture : close_in ? le i = 'numero de fichier', s ="grids/grid" ou "solutions/solution" *)
+let l i s = 
+  let l1 = (s ^ Char.escaped i) in 
+  let l2 = ( l1^ Char.escaped '.') in 
+  let l3  = (l2^ Char.escaped 't')in 
+  let l4  = (l3^ Char.escaped 'x') in 
+  let l5 = (l4^ Char.escaped 't') in 
+  let c = open_in l5 in 
+  input_line c;;
+
+
 (*	Retire nb chiffres de la table table et les remplaces par le charactere zéro
  *
  * 	in/ nb un entier compris entre 0 (pas d'effet) et le nombre max de valeur
  *		différentes de zéro
  *	in/ une table contenant l'ensemble
  *)
-
-
 let retireChiffres nb table =
 	let rec rc k =
 		match k with
