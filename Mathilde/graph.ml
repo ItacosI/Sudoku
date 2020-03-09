02536199acbcbac0ef9860ce689719694e47e647
MathildeVerriez <47179976+MathildeVerriez@users.noreply.github.com>
2020-03-09 15:16:14 +0100

    Update graph.ml

--- Mathilde/graph.ml
+++ Mathilde/graph.ml
@@ -4,39 +4,72 @@ let height = 700;;
 
 open_graph " 1280x720";;
 
-
+(*
 let dessineMenu =
 	Graphics.fill_arc 640 700 350 50 0 (-180);
 	Graphics.fill_arc 640 0 350 50 0 180;;
-
-
+	*)
 
 
 let menuDebut =
+(*
 	Graphics.set_font "-*-fixed-medium-r-*--24-*-*-*-*-*-iso8859-1";
 	Graphics.moveto 580 620;
 	Graphics.draw_string "BIENVENUE";
 	Graphics.moveto 450 500;
 	Graphics.draw_string "Quelle grille voulez-vous (0 - 243) :";
 	Graphics.moveto 900 500;
-	Graphics.draw_string ".";
 	Graphics.moveto 450 400;
 	Graphics.draw_string "Difficulte (0 - 40) : ";
 	Graphics.moveto 707 400;
-	Graphics.draw_string ".";
 	Graphics.moveto 450 200;
 	fill_rect 540 202 20 20;
 	Graphics.draw_string "ENTREE"
-	;;
+	;;*)
+	draw_image(Image.init_image "yol.ppm") 0 0;;
 
 	let reponseJ =
-	let b = ref true in
-		while !b do
-	 	let s = Graphics.wait_next_event [Graphics.Button_down]
-			in match s.Graphics.mouse_y with
-				|a when s.Graphics.mouse_y > 490 && s.Graphics.mouse_y < 520 -> if s.Graphics.mouse_x > 890 && s.Graphics.mouse_x < 910  then begin Graphics.moveto 900 500; Graphics.draw_char s.Graphics.key ; Printf.printf"%c" s.Graphics.key end
-				|b when s.Graphics.mouse_y > 390 && s.Graphics.mouse_y < 420 -> if s.Graphics.mouse_x > 690 && s.Graphics.mouse_x < 720  then begin Graphics.moveto 472 400; Graphics.draw_char s.Graphics.key ; Printf.printf"%c" s.Graphics.key end
-				|c when s.Graphics.mouse_y > 150 && s.Graphics.mouse_y < 250 -> if s.Graphics.mouse_x > 500 && s.Graphics.mouse_x < 800 then b:= false;
+	let ba = ref true in
+	let nbNom = ref 10 in
+	let nbGrille = ref 3 in
+	let nbDiffuculte = ref 2 in
+	let u = ref 720 in
+	let v = ref 720 in
+	let w =  ref 720 in
+		while !ba do
+		 	let s = Graphics.wait_next_event [Graphics.Button_down]
+				in match s.Graphics.mouse_y with
+					|nom when s.Graphics.mouse_y > 445 && s.Graphics.mouse_y < 495 -> if s.Graphics.mouse_x > (!u - 50) && s.Graphics.mouse_x < (!u + 50)  then
+																										begin
+																											while !nbNom > 0 do
+																												begin
+																												Graphics.moveto !u 460; nbNom := (!nbNom - 1) ; let key = read_key () in if int_of_char(key) = 13 then nbNom := 0 else Graphics.draw_char key ; u:=!u +15
+																												end
+																										 done
+																										end
+
+
+					|grille when s.Graphics.mouse_y > 345 && s.Graphics.mouse_y < 395 -> if s.Graphics.mouse_x > (!v - 50) && s.Graphics.mouse_x < (!v + 50)  then
+																										begin
+																											while !nbGrille > 0 do
+																												begin
+																												Graphics.moveto !v 360; nbGrille := (!nbGrille - 1); let key = read_key () in if int_of_char(key) = 13 then nbGrille := 0 else Graphics.draw_char key ; v:=!v +15
+																												end
+																										 done
+																										end
+
+
+					|difficulte when s.Graphics.mouse_y > 245 && s.Graphics.mouse_y < 295 -> if s.Graphics.mouse_x > (!w - 50)&& s.Graphics.mouse_x <(!w + 50) then
+																											begin
+																												while !nbDiffuculte > 0 do
+																													begin
+																													Graphics.moveto !w 260;nbDiffuculte := (!nbDiffuculte - 1); let key = read_key () in if int_of_char(key) = 13 then nbDiffuculte := 0 else Graphics.draw_char key ; w:=!w +15
+																													end
+																										 		done
+																											end
+
+					|valide when s.Graphics.mouse_y > 125 && s.Graphics.mouse_y < 175-> if s.Graphics.mouse_x > 540 && s.Graphics.mouse_x < 740 then begin Graphics.clear_graph(); ba:= false end
+					|_ -> ();
 			done
 		;;
 
@@ -61,7 +94,6 @@ let menuDebut =
 				Graphics.moveto (120 + (case mod 9)*55 + ((case mod 9)/3)*3) (560 - (case/9)*55 - (case/27)*3);
 
 			if ((int_of_char (valeur)-48) != 0) then begin
-
 			  Graphics.draw_string (String.make 1 valeur) ;
 			end
 			else
@@ -75,22 +107,14 @@ let menuDebut =
 
 
 			let dessineSudoku grille =
-
 			for i = 0 to 80 do
 				dessineNb i grille.[i];
 			done;;
 
 
 
-
-
-
-
-
-
-
 dessineGrille;;
-dessineMenu;;
+(*dessineMenu;;*)
 dessineSudoku grillesudokuB;;
 
 Printf.printf "%d" Graphics.foreground;
